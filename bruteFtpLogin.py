3f43424e691645063241768f7a189616de3f7abe
Noah <51211511+iamrrv01@users.noreply.github.com>
2021-09-12 21:53:41 +0530

    Update bruteFtpLogin.py

--- bruteFtpLogin.py
+++ bruteFtpLogin.py
@@ -2,7 +2,7 @@ print("  _ ___ _      _                                                         
 print(" |_  | |_) __ /  ._ _.  _ |   _  ._   _|_  _   _  |   o  _   ._ _   _.  _|  _    |_       |_) o _|_     ._ _. o  / |\ |  _   _. |_  ")
 print(" |   | |      \_ | (_| (_ |< (/_ |     |_ (_) (_) |   | _>   | | | (_| (_| (/_   |_) \/   | \ |  |_ |_| | (_| | /  | \| (_) (_| | | ")
 
-print("[!] legal disclaimer: Usage of sqlmap for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program.")
+print("[!] legal disclaimer: Usage of ftp-crack for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program.")
 print(" ")
 import ftplib
 import socket
