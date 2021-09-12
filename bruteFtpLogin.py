4371219cae959136f49f129fdaa21152d45c5dba
Noah <51211511+iamrrv01@users.noreply.github.com>
2021-09-12 21:43:51 +0530

    ftplogin.py

--- bruteFtpLogin.py
+++ bruteFtpLogin.py
@@ -1,6 +1,6 @@
-print("\t  _ ___ _      _                                                                           _                                        ")
-print("\t |_  | |_) __ /  ._ _.  _ |   _  ._   _|_  _   _  |   o  _   ._ _   _.  _|  _    |_       |_) o _|_     ._ _. o  / |\ |  _   _. |_  ")
-print("\t |   | |      \_ | (_| (_ |< (/_ |     |_ (_) (_) |   | _>   | | | (_| (_| (/_   |_) \/   | \ |  |_ |_| | (_| | /  | \| (_) (_| | | ")
+print("  _ ___ _      _                                                                           _                                        ")
+print(" |_  | |_) __ /  ._ _.  _ |   _  ._   _|_  _   _  |   o  _   ._ _   _.  _|  _    |_       |_) o _|_     ._ _. o  / |\ |  _   _. |_  ")
+print(" |   | |      \_ | (_| (_ |< (/_ |     |_ (_) (_) |   | _>   | | | (_| (_| (/_   |_) \/   | \ |  |_ |_| | (_| | /  | \| (_) (_| | | ")
 
 print("[!] legal disclaimer: Usage of sqlmap for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program.")
 print(" ")
